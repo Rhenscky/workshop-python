cb68945d5a52a3feed84480dde2a6cb5df5f7a74
195410003 Rhenscky Cokrowarno Yamwav <72866770+Rhenscky@users.noreply.github.com>
2022-02-26 11:03:34 +0700

    Add files via upload

---
+++ minggu-02/src/1.py
@@ -0,0 +1,11 @@
+x = int(input("Masukkan bilangan bulat : "))
+if x < 0:
+    x = 0
+    print('Mengubah negatif menjadi nol')
+    elif x == 0:
+        print('Nol')
+        elif x == 1:
+            print('Satu')
+            else:
+                print('Lebih')
+
