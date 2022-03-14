9a7b9344ad918870f9612a37e2dabd1bd7180b4a
195410003 Rhenscky Cokrowarno Yamwav <72866770+Rhenscky@users.noreply.github.com>
2022-03-14 16:44:03 +0700

    Update Kode 13.py

--- minggu-05/src/Kode 13.py
+++ minggu-05/src/Kode 13.py
@@ -3,7 +3,6 @@ print('The value of pi is approximately %5.3f.' % math.pi)
 f = open('workfile', 'w')
 with open('workfile') as f:
     read_data = f.read()
-# We can check that the file has been automatically closed.
 f.closed
 f.close()
 f.read()
