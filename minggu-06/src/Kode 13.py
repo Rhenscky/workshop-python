5785414bea3ac5229d64e1e1839b190036bf6d6f
195410003 Rhenscky Cokrowarno Yamwav <72866770+Rhenscky@users.noreply.github.com>
2022-03-21 21:57:53 +0700

    Add files via upload

---
+++ minggu-06/src/Kode 13.py
@@ -0,0 +1,6 @@
+def func():
+    raise ConnectionError
+try:
+    func()
+except ConnectionError as exc:
+    raise RuntimeError('Failed to open database') from exc
---
+++ minggu-06/src/Kode 9.py
@@ -0,0 +1,6 @@
+def this_fails():
+    x = 1/0
+try:
+    this_fails()
+except ZeroDivisionError as err:
+    print('Handling run-time error:', err)
